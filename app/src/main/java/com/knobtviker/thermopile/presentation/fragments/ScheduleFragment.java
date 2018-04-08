bc4187abbb88fc2b47e56380228c35a8e4b0f4b4
Bojan <bojan.komljenovic@7of9.agency>
2018-04-08 20:42:12 +0200

    Simplified schedule layout.

--- app/src/main/java/com/knobtviker/thermopile/presentation/fragments/ScheduleFragment.java
+++ app/src/main/java/com/knobtviker/thermopile/presentation/fragments/ScheduleFragment.java
@@ -15,7 +15,6 @@ import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewConfiguration;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 import com.knobtviker.thermopile.R;
 import com.knobtviker.thermopile.data.models.local.Settings;
@@ -23,7 +22,6 @@ import com.knobtviker.thermopile.data.models.local.Threshold;
 import com.knobtviker.thermopile.presentation.contracts.ScheduleContract;
 import com.knobtviker.thermopile.presentation.fragments.implementation.BaseFragment;
 import com.knobtviker.thermopile.presentation.presenters.SchedulePresenter;
-import com.knobtviker.thermopile.presentation.utils.Constants;
 import com.knobtviker.thermopile.presentation.utils.Router;
 import com.knobtviker.thermopile.presentation.views.communicators.MainCommunicator;
 import com.knobtviker.thermopile.presentation.views.viewholders.ThresholdViewHolder;
@@ -32,7 +30,6 @@ import org.joda.time.DateTime;
 import org.joda.time.Minutes;
 
 import java.util.List;
-import java.util.Locale;
 import java.util.stream.IntStream;
 
 import butterknife.BindViews;
@@ -49,12 +46,6 @@ import static android.support.constraint.ConstraintSet.TOP;
 
 public class ScheduleFragment extends BaseFragment<ScheduleContract.Presenter> implements ScheduleContract.View {
     public static final String TAG = ScheduleFragment.class.getSimpleName();
-
-    @BindViews({R.id.textview_hour_00, R.id.textview_hour_01, R.id.textview_hour_02, R.id.textview_hour_03, R.id.textview_hour_04, R.id.textview_hour_05, R.id.textview_hour_06, R.id.textview_hour_07, R.id.textview_hour_08, R.id.textview_hour_09, R.id.textview_hour_10, R.id.textview_hour_11, R.id.textview_hour_12, R.id.textview_hour_13, R.id.textview_hour_14, R.id.textview_hour_15, R.id.textview_hour_16, R.id.textview_hour_17, R.id.textview_hour_18, R.id.textview_hour_19, R.id.textview_hour_20, R.id.textview_hour_21, R.id.textview_hour_22, R.id.textview_hour_23})
-    public List<TextView> textViewHours;
-
-    @BindViews({R.id.textview_minute_00, R.id.textview_minute_01, R.id.textview_minute_02, R.id.textview_minute_03, R.id.textview_minute_04, R.id.textview_minute_05, R.id.textview_minute_06, R.id.textview_minute_07, R.id.textview_minute_08, R.id.textview_minute_09, R.id.textview_minute_10, R.id.textview_minute_11, R.id.textview_minute_12, R.id.textview_minute_13, R.id.textview_minute_14, R.id.textview_minute_15, R.id.textview_minute_16, R.id.textview_minute_17, R.id.textview_minute_18, R.id.textview_minute_19, R.id.textview_minute_20, R.id.textview_minute_21, R.id.textview_minute_22, R.id.textview_minute_23})
-    public List<TextView> textViewMinutes;
 
     @BindViews({R.id.layout_hours_monday, R.id.layout_hours_tuesday, R.id.layout_hours_wednesday, R.id.layout_hours_thursday, R.id.layout_hours_friday, R.id.layout_hours_saturday, R.id.layout_hours_sunday})
     public List<ConstraintLayout> hourLayouts;
@@ -119,21 +110,21 @@ public class ScheduleFragment extends BaseFragment<ScheduleContract.Presenter> i
 
     @Override
     public void onSettingsChanged(@NonNull Settings settings) {
-        IntStream
-            .range(0, textViewMinutes.size())
-            .forEach(i -> {
-                if (settings.formatClock() == Constants.CLOCK_MODE_12H) {
-                    if (i == 0) {
-                        textViewHours.get(i).setText(String.valueOf(i + 12));
-                    } else {
-                        textViewHours.get(i).setText(i < 13 ? (i < 10 ? String.format(Locale.getDefault(), "0%d", i) : String.valueOf(i)) : (i - 12 < 10 ? String.format(Locale.getDefault(), "0%d", i - 12) : String.valueOf(i - 12)));
-                    }
-                    textViewMinutes.get(i).setText(getString(i < 12 ? R.string.am : R.string.pm));
-                } else {
-                    textViewHours.get(i).setText(i < 10 ? String.format(Locale.getDefault(), "0%d", i) : String.valueOf(i));
-                    textViewMinutes.get(i).setText(getString(R.string._00));
-                }
-            });
+//        IntStream
+//            .range(0, textViewMinutes.size())
+//            .forEach(i -> {
+//                if (settings.formatClock() == Constants.CLOCK_MODE_12H) {
+//                    if (i == 0) {
+//                        textViewHours.get(i).setText(String.valueOf(i + 12));
+//                    } else {
+//                        textViewHours.get(i).setText(i < 13 ? (i < 10 ? String.format(Locale.getDefault(), "0%d", i) : String.valueOf(i)) : (i - 12 < 10 ? String.format(Locale.getDefault(), "0%d", i - 12) : String.valueOf(i - 12)));
+//                    }
+//                    textViewMinutes.get(i).setText(getString(i < 12 ? R.string.am : R.string.pm));
+//                } else {
+//                    textViewHours.get(i).setText(i < 10 ? String.format(Locale.getDefault(), "0%d", i) : String.valueOf(i));
+//                    textViewMinutes.get(i).setText(getString(R.string._00));
+//                }
+//            });
     }
 
     @Override
