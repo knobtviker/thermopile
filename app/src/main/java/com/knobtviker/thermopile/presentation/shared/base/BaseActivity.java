c8c0fce88fd4dea2291dcf643321eb60bc1a117e
Bojan <knobtviker@gmail.com>
2018-09-10 21:41:01 +0200

    Code cleanup.

--- app/src/main/java/com/knobtviker/thermopile/presentation/shared/base/BaseActivity.java
+++ app/src/main/java/com/knobtviker/thermopile/presentation/shared/base/BaseActivity.java
@@ -2,8 +2,9 @@ package com.knobtviker.thermopile.presentation.shared.base;
 
 import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.preference.PreferenceManager;
 import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.app.AppCompatDelegate;
 import android.view.MotionEvent;
 
@@ -12,21 +13,28 @@ import com.knobtviker.thermopile.presentation.activities.ScreenSaverActivity;
 import com.knobtviker.thermopile.presentation.shared.constants.integrity.Default;
 import com.knobtviker.thermopile.presentation.shared.constants.integrity.Preferences;
 
-import dagger.android.support.DaggerAppCompatActivity;
+import javax.inject.Inject;
+
+import dagger.android.AndroidInjection;
+import dagger.android.AndroidInjector;
+import dagger.android.DispatchingAndroidInjector;
+import dagger.android.support.HasSupportFragmentInjector;
 
 /**
  * Created by bojan on 10/06/2017.
  */
 
-public abstract class BaseActivity extends DaggerAppCompatActivity {
+public abstract class BaseActivity extends AppCompatActivity implements HasSupportFragmentInjector {
 
-//    @Inject
-    public SharedPreferences sharedPreferences;
+    @Inject
+    DispatchingAndroidInjector<Fragment> supportFragmentInjector;
+
+    @Inject
+    SharedPreferences sharedPreferences;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
-
-        sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this.getApplicationContext());
+        AndroidInjection.inject(this);
 
         AppCompatDelegate.setDefaultNightMode(
             sharedPreferences.getInt(Preferences.THEME, Default.THEME)
@@ -51,5 +59,10 @@ public abstract class BaseActivity extends DaggerAppCompatActivity {
         }
 
         return super.dispatchTouchEvent(event);
+    }
+
+    @Override
+    public AndroidInjector<Fragment> supportFragmentInjector() {
+        return supportFragmentInjector;
     }
 }
